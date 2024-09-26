d02b0c87910b6a8eb00deb1a6bcb87a740d4e940
shahd-gamel <shahd3129@gmail.com>
2024-09-26 21:53:10 +0300

    Create ADC_PRIV.h

---
+++ ADC_PRIV.h
@@ -0,0 +1,11 @@
+#ifndef MCAL_ADC_ADC_PRIVET_H_
+#define MCAL_ADC_ADC_PRIVET_H_
+
+#define ADMUX    *((volatile U8*)0X27)
+#define ADCSRA   *((volatile U8*)0X26)
+#define ADCH     *((volatile U8*)0X25)
+#define ADCL     *((volatile U8*)0X24)
+#define SFIOR    *((volatile U8*)0X50)
+
+
+#endif /* MCAL_ADC_ADC_PRIVET_H_ */
