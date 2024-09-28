578aa8ae6a8c6ae9875a5032236b1f04ca700b6b
shahd-gamel <shahd3129@gmail.com>
2024-09-28 18:12:38 +0300

    Create STD_TYPES.h

---
+++ STD_TYPES.h
@@ -0,0 +1,19 @@
+#ifndef _STD_TYPES_H
+#define _STD_TYPES_H
+
+typedef unsigned char U8;
+typedef unsigned short int U16;
+typedef unsigned int U32;
+typedef unsigned long long int  U64;
+
+
+typedef signed char S8;
+typedef signed short int S16;
+typedef signed int S32;
+typedef signed long long int  S64;
+
+
+typedef float F32;
+typedef double F64;
+
+#endif
